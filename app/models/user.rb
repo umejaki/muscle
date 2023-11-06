6df47d3a146522066a1eae0e8027aae49ecac9e6
Daiki Umezawa <137897494+umejaki@users.noreply.github.com>
2023-11-06 12:36:23 +0000

    fix remove null_false nick_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  validates :nick_name, presence: true
+         
   has_many :post_images, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
